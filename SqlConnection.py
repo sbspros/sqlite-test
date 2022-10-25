5560bfe545a595e92a7a7a7efd2d61f462069fa2
Richard Chamberlain <59230129+sbspros@users.noreply.github.com>
2022-10-25 07:03:56 -0600

    Update SqlConnection.py

--- SqlConnection.py
+++ SqlConnection.py
@@ -1,11 +1,28 @@
 import sqlite3
+from common.BaseClass import BaseClass
+import traceback
+
+__author__ = "Richard Chamberlain"
+__copyright__ = "Copyright 2022"
+__license__ = "MIT"
+__version__ = "1.0"
+__maintainer__ = "Richard Chamberlain"
+__email__ = "richard@sbspros.ca"
+__status__ = "Dev"
+
+
+class ConnectFailed(Exception):
+    def __init__(self):
+        self.msg = 'Media Finder failed to initialize'
+        super().__init__(self.msg)
 
 
 class SQLConnection():
 
-  def __init__(self,business_object:str,database:str)
+  def __init__(self,bc:BaseClass,business_object:str,database:str)
+    self._bc=bc
     try:
       self._conn=sqlite3.connect(database)
     except:
-      
+      raise ConnectFailed
      
