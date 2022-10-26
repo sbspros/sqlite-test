3488e6161e6d875e49af0f8437134bcd7ca9285a
Richard Chamberlain <59230129+sbspros@users.noreply.github.com>
2022-10-26 11:51:00 -0600

    Create Section.py

---
+++ Section.py
@@ -0,0 +1,11 @@
+class PlexSection():
+  def __init__(self):
+    self._id=None
+    self._name=None
+    self._refresh_flag=False
+    self._last_scan=None
+    self._last_reviewed=None
+    self._date_created=None
+    self._date_modified=None
+          
+     
